08b82e2fb53439e59a054dd5b2fd5897da02b781
simran-bhadani3 <simranbhadani333@gmail.com>
2023-02-25 12:38:49 +0800

    uncomment

--- src/controller/claims/index.js
+++ src/controller/claims/index.js
@@ -1,15 +1,15 @@
-// const schemes = require('../models/claim');
-// module.exports.insert = async(res, params) => {
-//     const newClaim = new Claim({
-//
-//     })
-//
-//     try {
-//         return res.success(201).json({message: "Claim created successfully"});
-//     } catch (error) {
-//         return res.status(400).json({
-//             status: 400,
-//             message: error
-//         })
-//     }
-// }
+const schemes = require('../models/claim');
+module.exports.insert = async(res, params) => {
+    const newClaim = new Claim({
+
+    })
+
+    try {
+        return res.success(201).json({message: "Claim created successfully"});
+    } catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: error
+        })
+    }
+}
