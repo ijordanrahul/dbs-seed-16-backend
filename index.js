4768da36d2a0b324ab9d8b7316e8d72ea7f93df9
simran-bhadani3 <simranbhadani333@gmail.com>
2023-02-25 11:15:13 +0800

    setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config');
+
+const index = express();
+
+index.use(bodyParser.json());
+index.use(bodyParser.urlencoded({ extended: false }));
+
+const app = index.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,content-type'
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+const PORT = process.env.PORT || config.port;
+
+const server = app.listen(PORT, () => {
+    console.log('server is running on port', server.address().port);
+});
