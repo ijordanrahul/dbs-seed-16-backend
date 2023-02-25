825a45e1c6ec2e6f70571886e57bbfe244184ff3
Hendey Fan <hendeyfan98@gmail.com>
2023-02-25 15:29:22 +0800

    temp update

--- src/controller/claims/index.js
+++ src/controller/claims/index.js
@@ -20,10 +20,10 @@ module.exports.retrieveClaimsLimited = async(req, res) => {
     console.log("hell")
     policies = await PolicySchema.find({employeeId: "63f99f8bb23b33b81d61cd49"}).exec()
     // user = await UserSchema.findById(mongoose.Types.ObjectId(userid)).exec();
-    claims = await InsuranceClaimSchema.findById(mongoose.Types.ObjectId('63f9b4929717f7fc3aecf431')).exec();
+    // claims = await InsuranceClaimSchema.findById(mongoose.Types.ObjectId('63f9b4929717f7fc3aecf431')).exec();
     // claims = await InsuranceClaimSchema.find({insuranceId: "63f9ab7f9717f7fc3aecf430"}).exec()
     console.log(policies)
-    console.log(claims)
+    // console.log(claims)
     // claim = await InsuranceClaimSchema.find({}).exec();
     return res.status(200).json({
         status: true,
