ea651f121016602bf9351d6840e41d574d6c2e6d
ijordanrahul <65341570+ijordanrahul@users.noreply.github.com>
2023-02-25 15:56:56 +0800

    get_all_policies_updated

--- src/controller/policies/index.js
+++ src/controller/policies/index.js
@@ -4,24 +4,25 @@ const Policy = require("../../models/Policy");
 
 
 module.exports.retrievePolicy = async(req, res) => {
-    const { employeeID } = req.body;
-    console.log(req.body)
-    employee = await PolicySchema.findById(mongoose.Types.ObjectId(employeeID)).exec();
-    console.log(employee)
+    const employeeID  = req.body.employeeId;
+    console.log(employeeID)
+    employee_policies = await PolicySchema.find({ employeeId: employeeID }).exec();
+    console.log(employee_policies)
+    return res.status(201).json({employee_policies});
 }
-
+ 
 module.exports.insertPolicy = async(params, res) => {
 
     console.log(params.body)
 
     const newPolicy = new Policy({
-        employeeId: params.employeeId,
-        insuranceType: params.insuranceType,
-        policyStartDate: params.policyStartDate,
-        policyTerm: params.policyTerm,
-        policyEndDate: params.policyEndDate,
-        claimLimit: params.claimLimit,
-        remainingClaimLimit: params.remainingClaimLimit,
+        employeeId: params.body.employeeId,
+        insuranceType: params.body.insuranceType,
+        policyStartDate: params.body.policyStartDate,
+        policyTerm: params.body.policyTerm,
+        policyEndDate: params.body.policyEndDate,
+        claimLimit: params.body.claimLimit,
+        remainingClaimLimit: params.body.remainingClaimLimit,
     })
 
     console.log(newPolicy)
