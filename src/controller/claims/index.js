1f65f6801f8b0521c0fa701686e8240a6c9f6c8f
Hendey Fan <hendeyfan98@gmail.com>
2023-02-25 13:54:36 +0800

    init connection to db code

--- src/controller/claims/index.js
+++ src/controller/claims/index.js
@@ -1,4 +1,7 @@
 const schemes = require('../../models/InsuranceClaims');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://cs3219:ineedapassword@cluster0.d5qufy7.mongodb.net/?retryWrites=true&w=majority";
+    
 // module.exports.insert = async(res, params) => {
 //     const newClaim = new Claim({
 
@@ -16,6 +19,14 @@ const schemes = require('../../models/InsuranceClaims');
 
 module.exports.retrieveClaimsLimited = async(req, res) => {
     const { eId } = req.query;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    client.connect(err => {
+      const collection = client.db("cs3219").collection("user");
+      // perform actions on the collection object
+      console.log(collection)
+      client.close();
+    });
+
     return res.status(200).json({
         status: true,
         testString: "teststring",
