1f58410ef4a872d20f88a19256a8bc0713a7c14d
a180024 <a180024@e.ntu.edu.sg>
2023-02-25 13:53:58 +0800

    Update auth

--- src/controller/users/index.js
+++ src/controller/users/index.js
@@ -1,8 +1,9 @@
+const mongoose = require("mongoose");
 const UserSchema = require("../../models/User");
 
-module.exports.logIn = async (res, parameters) => {
-  const { username, password } = parameters;
-  console.log(await UserSchema.find({}).exec());
+module.exports.logIn = async (req, res) => {
+  const { userid, password } = req.body;
+  user = await UserSchema.findById(mongoose.Types.ObjectId(userid)).exec();
   // schemes.User.findOne({ username: username }, function (err, user) {
   // if (err) return res.status(400).json({ status: 400, message: err });
   // Bcrypt.compare(password, user.password, (err, success) => {
@@ -19,4 +20,28 @@ module.exports.logIn = async (res, parameters) => {
   // }
   // });
   // });
+};
+
+module.exports.signUp = async (req, res) => {
+  const { firstname, lastname, password, age } = req.body;
+  const newUser = UserSchema({
+    password,
+    age,
+    firstname,
+    lastname,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    return res.status(201).json({
+      id: new String(savedUser._id),
+      firstname: savedUser.firstname,
+      lastname: savedUser.lastname,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 400,
+      message: "Failed to create user!",
+    });
+  }
 };
--- src/controller/users/routes.js
+++ src/controller/users/routes.js
@@ -4,8 +4,12 @@ const controller = require("./index");
 
 const router = express.Router();
 
-router.post("/api/auth", (req, res) => {
+router.post("/api/logIn", (req, res) => {
   controller.logIn(req, res);
+});
+
+router.post("/api/signUp", (req, res) => {
+  controller.signUp(req, res);
 });
 
 module.exports = router;
